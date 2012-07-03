9e300fc4f3485e24254d5fbba25b3bc372e8b169
Clay Walker <ClaytonWalker11@gmail.com>
2012-07-02 21:06:16 -0600

    This contains minor tweaks to RefreshAlerts.java and ClassAlertServiceImpl.java

--- src/edu/utah/is6460/server/ClassAlertServiceImpl.java
+++ src/edu/utah/is6460/server/ClassAlertServiceImpl.java
@@ -1,4 +1,5 @@
 package edu.utah.is6460.server;
+
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -37,52 +38,53 @@ public class ClassAlertServiceImpl extends RemoteServiceServlet implements
 		ClassAlertService {
 
 	@Override
-	public String addClass(String term, String subject, String catalogNum, String email, String section) throws IllegalArgumentException {
-		Long seats = askYahoo(term, subject, catalogNum, email, section);
+	public String addClass(String term, String subject, String catalogNum,
+			String email, String section) throws IllegalArgumentException {
+		Long seats = askYahoo(term, subject, catalogNum, section);
 		String result = "";
-		if(seats.equals(0L)){
+		if (seats.equals(0L)) {
 			storeAlert(term, subject, catalogNum, email, section);
-			//RETURN message saying that we will email them
+			// RETURN message saying that we will email them
 			result = "request saved. you will be emailed with a seat is available";
-		}else if(seats.equals(-1L)){
-			//RETURN message saying that there was an error, check input
+		} else if (seats.equals(-1L)) {
+			// RETURN message saying that there was an error, check input
 			result = "There was a problem finding the class. Please check your input.";
-		}else if(seats >= 1){
-			//RETURN message saying that there are currently seats available
-			emailMessage(email);
+		} else if (seats >= 1) {
+			// RETURN message saying that there are currently seats available
 			result = "There is currently an opening for that class... go sign up... quick!";
 		}
 		return result;
 	}
 
-	private void emailMessage(String email) {
+	public static void emailMessage(String email) {
 		Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
+		Session session = Session.getDefaultInstance(props, null);
 
-        String msgBody = "This is a test email from appengine";
+		String msgBody = "This is a test email from appengine";
 
-        try {
-            Message msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress("narfdre@gmail.com", "Class Alert"));
-            msg.addRecipient(Message.RecipientType.TO,
-                             new InternetAddress(email, "Mr. User"));
-            msg.setSubject("There is a seat available in your class");
-            msg.setText(msgBody);
-            Transport.send(msg);
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress("narfdre@gmail.com", "Class Alert"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					email, "Mr. User"));
+			msg.setSubject("There is a seat available in your class");
+			msg.setText(msgBody);
+			Transport.send(msg);
 
-        } catch (AddressException e) {
-        	e.printStackTrace();
-            // ...
-        } catch (MessagingException e) {
-        	e.printStackTrace();
-            // ...
-        } catch (UnsupportedEncodingException e) {
-        	e.printStackTrace();
-        	// ...
-        }
+		} catch (AddressException e) {
+			e.printStackTrace();
+			// ...
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			// ...
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			// ...
+		}
 	}
 
-	private Long askYahoo(String term, String subject, String catalogNum, String email, String section){
+	public static Long askYahoo(String term, String subject, String catalogNum,
+			String section) {
 		Long seats = -1L;
 		try {
 			String urlQuery = "http://query.yahooapis.com/v1/public/yql"
@@ -97,25 +99,26 @@ public class ClassAlertServiceImpl extends RemoteServiceServlet implements
 					+ "catno%3D"
 					+ URLEncoder.encode(catalogNum, "UTF-8")
 					+ "%22%20and%0A%20%20%20%20%20%20xpath%3D'%2F%2Ftable%2Ftr%5Btd%2F%2Ftext()%5Bcontains(.%2C%20%22"
-					+ URLEncoder.encode(section, "UTF-8") 
+					+ URLEncoder.encode(section, "UTF-8")
 					+ "%22)%5D%5D%2Ftd%5B8%5D'&format=xml";
-			
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(urlQuery);
 			doc.getDocumentElement().normalize();
 			NodeList nodeList = doc.getElementsByTagName("font");
 			Node node = nodeList.item(0);
 			seats = Long.valueOf(node.getTextContent());
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return seats;
-		
+
 	}
-	
+
 	private void storeAlert(String term, String subject, String catalogNum,
 			String email, String section) {
 		Key classalertKey = KeyFactory.createKey("AlertGroup", email);
@@ -127,6 +130,7 @@ public class ClassAlertServiceImpl extends RemoteServiceServlet implements
 		alert.setProperty("email", email);
 		alert.setProperty("section", section);
 		alert.setProperty("dateAdded", date);
+		alert.setProperty("flag", "0");
 		DatastoreService datastore = DatastoreServiceFactory
 				.getDatastoreService();
 		datastore.put(alert);
@@ -134,28 +138,28 @@ public class ClassAlertServiceImpl extends RemoteServiceServlet implements
 
 	private String getAlert(String email) {
 		String catalog = "";
-		
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		
-		Query q = new Query("Alert");
-		
-		//q.addFilter("email", Query.FilterOperator.EQUAL, email);
-		
-		PreparedQuery pq = datastore.prepare(q);
-		
-		for (Entity result : pq.asList(FetchOptions.Builder.withDefaults())){
-			//System.out.println(result);
-			catalog = (String) result.getProperty("catalog");;
-		}
-		return catalog;
-		
-		/*Entity e = new Entity("Alert");
+
 		DatastoreService datastore = DatastoreServiceFactory
 				.getDatastoreService();
-		try {
-			e = datastore.get(KeyFactory.createKey("AlertGroup", email));
-		} catch (Exception ex) {
+
+		Query q = new Query("Alert");
+
+		// q.addFilter("email", Query.FilterOperator.EQUAL, email);
+
+		PreparedQuery pq = datastore.prepare(q);
+
+		for (Entity result : pq.asList(FetchOptions.Builder.withDefaults())) {
+			// System.out.println(result);
+			catalog = (String) result.getProperty("catalog");
+			;
 		}
-		return e.toString();*/
+		return catalog;
+
+		/*
+		 * Entity e = new Entity("Alert"); DatastoreService datastore =
+		 * DatastoreServiceFactory .getDatastoreService(); try { e =
+		 * datastore.get(KeyFactory.createKey("AlertGroup", email)); } catch
+		 * (Exception ex) { } return e.toString();
+		 */
 	}
 }
--- src/edu/utah/is6460/server/RefreshAlerts.java
+++ src/edu/utah/is6460/server/RefreshAlerts.java
@@ -13,27 +13,39 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 public class RefreshAlerts extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		DatastoreService datastore = DatastoreServiceFactory
 				.getDatastoreService();
-
+		
 		Query q = new Query("Alert");
-
+		q.setFilter(new FilterPredicate("flag",Query.FilterOperator.EQUAL,"0"));
 		PreparedQuery pq = datastore.prepare(q);
-
+		
 		for (Entity result : pq.asList(FetchOptions.Builder.withDefaults())) {
-			//String addClass(String term, String subject, String catalogNum, String email, String section) throws IllegalArgumentException;
-			
+			// String addClass(String term, String subject, String catalogNum,
+			// String email, String section) throws IllegalArgumentException;
+
 			String term = result.getProperty("term").toString();
 			String subject = result.getProperty("subj").toString();
-			String catalogNum = result.getProperty("catalog").toString(); 
+			String catalogNum = result.getProperty("catalog").toString();
 			String section = result.getProperty("section").toString();
-			
-			//toDo: Call Andre's method to check the URL
-			
+
+			Long seats  = ClassAlertServiceImpl.askYahoo(term, subject, catalogNum, section);
+			if(seats > 0 ){
+				String email = result.getProperty("email").toString();
+				ClassAlertServiceImpl.emailMessage(email);
+				result.setProperty("flag", 1);
+			datastore.put(result);
+			}
+			else{
+				//String email = result.getProperty("email").toString();
+				//ClassAlertServiceImpl.emailMessage(email);
+			}
+
 		}
 	}
 
