6102dc2dd8f71d1ec6cf6f54c5133209d4c5df21
Andre Kradolfer <narfdre@gmail.com>
2012-07-08 22:35:03 -0600

    adding basic error handling

--- src/edu/utah/is6460/client/AddClassForm.java
+++ src/edu/utah/is6460/client/AddClassForm.java
@@ -1,6 +1,8 @@
 package edu.utah.is6460.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.dom.client.FieldSetElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -21,6 +23,7 @@ public class AddClassForm extends Composite implements HasText {
 
 	@UiField Button submitClass;
 	@UiField TextBox subjectBox, termBox, catalogBox, emailBox, sectionBox;
+	@UiField FieldSetElement results;
 	
 	public AddClassForm() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -30,29 +33,42 @@ public class AddClassForm extends Composite implements HasText {
 
 	@UiHandler("submitClass")
 	void onClick(ClickEvent e) {
-		String subject = subjectBox.getValue();
-		String term = termBox.getValue();
-		String catalog = catalogBox.getValue();
-		String email = emailBox.getValue();
-		String section = sectionBox.getValue();
-		classAlertService.addClass(term, subject, catalog, email, section, new AsyncCallback<String>() {
-
-			@Override
-			public void onFailure(Throwable caught) {
+		String subject = subjectBox.getValue().isEmpty() ? emptyField(subjectBox) : clearField(subjectBox);
+		String term = termBox.getValue().isEmpty()  ? emptyField(termBox) : clearField(termBox);
+		String catalog = catalogBox.getValue().isEmpty() ? emptyField(catalogBox) : clearField(catalogBox);
+		String email = emailBox.getValue().isEmpty() ? emptyField(emailBox) : clearField(emailBox);
+		String section = sectionBox.getValue().isEmpty() ? emptyField(sectionBox): clearField(sectionBox);
+		if(!subject.isEmpty() && !term.isEmpty() && !catalog.isEmpty() && !email.isEmpty() && !section.isEmpty()){
+			results.setInnerText("");
+			classAlertService.addClass(term, subject, catalog, email, section, new AsyncCallback<String>() {
+	
+				@Override
+				public void onFailure(Throwable caught) {
+					
+				}
+	
+				@Override
+				public void onSuccess(String result) {
+					results.setInnerText(result);
+					subjectBox.setValue("");
+					termBox.setValue("");
+					catalogBox.setValue("");
+					emailBox.setValue("");
+					sectionBox.setValue("");
+				}
 				
-			}
+			});
+		}
+	}
 
-			@Override
-			public void onSuccess(String result) {
-				Window.alert(result);
-				subjectBox.setValue("");
-				termBox.setValue("");
-				catalogBox.setValue("");
-				emailBox.setValue("");
-				sectionBox.setValue("");
-			}
-			
-		});
+	private String clearField(TextBox box) {
+		box.getElement().removeClassName("error");
+		return box.getValue();
+	}
+
+	private String emptyField(TextBox box) {
+		box.getElement().addClassName("error");
+		return "";
 	}
 
 	public void setText(String text) {
